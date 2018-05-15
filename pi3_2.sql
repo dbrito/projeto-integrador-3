9b08126ea64093a0a2fd98e736c86ea8798465be
Leticia Siqueira <l_siq@outlook.com>
2018-05-14 22:05:09 -0300

    novo banco com tabela de usuarios

---
+++ pi3_2.sql
@@ -0,0 +1,201 @@
+CREATE DATABASE  IF NOT EXISTS `projeto_integrador` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `projeto_integrador`;
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: projeto_integrador
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.1.29-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cliente`
+--
+
+DROP TABLE IF EXISTS `cliente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cliente` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cpf` varchar(45) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `data_nascimento` date NOT NULL,
+  `telefone` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `ativo` bit(1) NOT NULL DEFAULT b'1',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `cpf_UNIQUE` (`cpf`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cliente`
+--
+
+LOCK TABLES `cliente` WRITE;
+/*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
+INSERT INTO `cliente` VALUES (4,'616.959.819-01','Steve Stifler','1990-01-01','','','\0'),(5,'111.131.934-09','Paul Finch ','1991-02-02','','','\0'),(6,'151.162.615-14','Fogell Mclovin','1993-04-04','','','\0'),(7,'0175290458','Douglas dos Santos','1993-08-27','(11) 98094-0524','doug.dbrito@gmail.com',''),(9,'105754','Martin Luther King','1970-08-08','(11) 98094-0524','asdadad@gmail','');
+/*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `filial`
+--
+
+DROP TABLE IF EXISTS `filial`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `filial` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(20) NOT NULL,
+  `endereco` varchar(20) NOT NULL,
+  `numero` int(10) NOT NULL,
+  `cidade` varchar(20) NOT NULL,
+  `estado` varchar(20) NOT NULL,
+  `ativo` bit(1) NOT NULL DEFAULT b'1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `filial`
+--
+
+LOCK TABLES `filial` WRITE;
+/*!40000 ALTER TABLE `filial` DISABLE KEYS */;
+INSERT INTO `filial` VALUES (1,'Fast - Iguatemi','Avenida Faria Lima',2466,'São Paulo','SP',''),(2,'teste','teste',666,'Salvador','BA','');
+/*!40000 ALTER TABLE `filial` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `item_venda`
+--
+
+DROP TABLE IF EXISTS `item_venda`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `item_venda` (
+  `id_item_venda` int(11) NOT NULL AUTO_INCREMENT,
+  `id_produto` int(11) NOT NULL,
+  `quantidade` int(11) NOT NULL,
+  `id_venda` int(11) NOT NULL,
+  PRIMARY KEY (`id_item_venda`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `item_venda`
+--
+
+LOCK TABLES `item_venda` WRITE;
+/*!40000 ALTER TABLE `item_venda` DISABLE KEYS */;
+INSERT INTO `item_venda` VALUES (11,24,1,16),(12,25,3,16);
+/*!40000 ALTER TABLE `item_venda` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `produto`
+--
+
+DROP TABLE IF EXISTS `produto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `produto` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(45) DEFAULT NULL,
+  `marca` varchar(45) DEFAULT NULL,
+  `preco` double NOT NULL,
+  `quantidade` int(11) NOT NULL,
+  `categoria` varchar(45) DEFAULT NULL,
+  `descricao` longtext,
+  `ativo` bit(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_produto_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `produto`
+--
+
+LOCK TABLES `produto` WRITE;
+/*!40000 ALTER TABLE `produto` DISABLE KEYS */;
+INSERT INTO `produto` VALUES (23,'Inspiron 14 6000','Dell',2459,40,'Perfume','Execute melhor suas tarefas em um notebook ultraportátil de 14\" cheio de estilo. Repleto de potencial. Assim como você.','\0'),(24,'Galaxy J7 Prime','Samsung',800,29,'Perfume','Produto de excelente qualidade',''),(25,'JBL GO 3W','JBL',150,57,'Caixas de Som','','\0'),(26,'teste','teste',50,50,'Perfume','asdadsasd',''),(27,'teste ','marca teste',500,100,'Perfume','asdas a sdas ','');
+/*!40000 ALTER TABLE `produto` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  `cpf` varchar(255) NOT NULL,
+  `user` varchar(255) NOT NULL,
+  `pass` varchar(255) NOT NULL,
+  `ativo` bit(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=72 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (30,'andreas','4050-2','aborba','senha40',''),(50,'leticia','12345-7','lesiq','senha10',''),(55,'marina','1214-8','nina','senha15',''),(70,'douglas','2021-4','dbrito','senha20','');
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `venda`
+--
+
+DROP TABLE IF EXISTS `venda`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `venda` (
+  `id_venda` int(11) NOT NULL AUTO_INCREMENT,
+  `data_venda` date NOT NULL,
+  `id_cliente` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_venda`),
+  UNIQUE KEY `id_venda_UNIQUE` (`id_venda`),
+  KEY `fk_cliente_idx` (`id_cliente`),
+  CONSTRAINT `fk_cliente` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `venda`
+--
+
+LOCK TABLES `venda` WRITE;
+/*!40000 ALTER TABLE `venda` DISABLE KEYS */;
+INSERT INTO `venda` VALUES (16,'2017-12-09',5);
+/*!40000 ALTER TABLE `venda` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-05-14 21:46:17
